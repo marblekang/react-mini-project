c79e572473dd888a247aad3366c56fae72d29680
marblekang <kangbm930@gmail.com>
2022-06-24 18:13:34 +0900

    feat: 스타일링 작성
    Card 컴포넌트 스타일링 하는 CardStyle.js에 스타일 작성

---
+++ src/style/CardStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export const ContentBox = styled.div`
+  margin-top: 20px;
+  width: 80%;
+  display: block;
+  height: 100px;
+  background-color: #efecec;
+  border-radius: 5px;
+`;
