37b467298db75b139d5b16ed16c00948606faaca
marblekang <kangbm930@gmail.com>
2022-06-24 18:08:26 +0900

    feat: API 호출
    API 호출하는 getData 함수 파일 분리

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-export const getData = async (query) => {
-  const result = await axios.get(
+export const getData = async (query, page) => {
+  const data = axios.get(
     `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&page=${page}&api-key=${process.env.REACT_APP_API_KEY}`
   );
+
+  const result = await data;
+  return result;
 };
