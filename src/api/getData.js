09cef3d43c1a4f798c4cdaf98bc8886bf7859b15
marblekang <kangbm930@gmail.com>
2022-06-24 21:32:24 +0900

    chore: getData.js 수정
    getData 함수의 parameter 초기값 지정

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getData = async (query, page) => {
+export const getData = async (query, page = 1) => {
   const data = axios.get(
     `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&page=${page}&api-key=${process.env.REACT_APP_API_KEY}`
   );
