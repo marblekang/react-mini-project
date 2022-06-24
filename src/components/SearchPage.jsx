fb8e72c3a64d5654284a8ffc4ebf01b0e196faae
marblekang <kangbm930@gmail.com>
2022-06-24 21:33:25 +0900

    feat: 추가 데이터 불러오기 기능 개발
    버튼 클릭하면 page 에 값을 더하면서 추가 데이터 불러오는 기능 개발

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -9,34 +9,41 @@ function SearchPage() {
   const [shownValue, setShownValue] = useState("");
   const [news, setNews] = useState();
   const [page, setPage] = useState(1);
+  let newPageData = [];
   const handleInput = (e) => {
     setShownValue(e.target.value);
     setTimeout(() => {
       setValue(e.target.value);
     }, 1500);
   };
+
+  const handleButton = () => {
+    setPage((prev) => ++prev);
+  };
+
   useEffect(() => {
     if (value) {
       let newsData = getData(value, page);
       newsData.then((res) => {
-        console.log(res.data.response.docs);
-        setNews(res.data.response.docs);
-
-        //setPage((prev) => prev++);
+        newPageData = [...res.data.response.docs];
+        setNews((prev) => [...prev, ...newPageData]);
       });
     } else {
       setNews([]);
+      setPage(1);
     }
   }, [value, page]);
   return (
     <>
       <Link to="/clip">
         <button>Clip</button>
+        <span>page:{page}</span>
       </Link>
       <Container>
         <Input onChange={handleInput} value={shownValue}></Input>
         {Boolean(value) ? <Card news={news}></Card> : null}
       </Container>
+      <button onClick={handleButton}>more news</button>
     </>
   );
 }
