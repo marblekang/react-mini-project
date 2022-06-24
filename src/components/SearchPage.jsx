09b03f4b8e9fd7ebf4b3c4ba4a741e33d75fe86c
marblekang <kangbm930@gmail.com>
2022-06-24 18:10:36 +0900

    feat: API 호출하면 기사 데이터 전달
    API를 호출하면 기사 데이터를 Card.jsx에 props로  전달하는 코드 작성

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,19 +1,41 @@
+import { useEffect, useState } from "react";
+import { getData } from "../api/getData";
 import { Container, Input } from "../style/SearchPageStyle";
+import { Card } from "./Card";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 function SearchPage() {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
+  const [shownValue, setShownValue] = useState("");
+  const [news, setNews] = useState();
+  const [page, setPage] = useState(1);
   const handleInput = (e) => {
-    setValue(e.target.value);
+    setShownValue(e.target.value);
+    setTimeout(() => {
+      setValue(e.target.value);
+    }, 1500);
   };
+  useEffect(() => {
+    if (value) {
+      let newsData = getData(value, page);
+      newsData.then((res) => {
+        console.log(res.data.response.docs);
+        setNews(res.data.response.docs);
+
+        //setPage((prev) => prev++);
+      });
+    } else {
+      setNews([]);
+    }
+  }, [value, page]);
   return (
     <>
       <Link to="/clip">
         <button>Clip</button>
       </Link>
       <Container>
-        <Input onChange={handleInput} value={value}></Input>
+        <Input onChange={handleInput} value={shownValue}></Input>
+        {Boolean(value) ? <Card news={news}></Card> : null}
       </Container>
     </>
   );
